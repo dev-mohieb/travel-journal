90bbf864bf987d8a17f257f7603972f3cbfba5fb
Mohieb <dev.mohieb@gmail.com>
2023-05-02 15:57:36 +0300

    clear unused classes and adjust footer size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="flex h-14 items-center justify-center gap-2 bg-[#F55A5A] text-white">
+    <footer className="flex h-8 items-center justify-center gap-2 bg-[#F55A5A] text-white">
       <p className=" font-medium">
         Created by{" "}
         <a
